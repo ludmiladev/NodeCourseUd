9349e5617a26cb174c4852d5c8f010d5e7a8e097
ludmiladev <ljuda12@gmail.com>
2021-03-18 23:45:01 +0200

    експорт через модуль, і імпорт через require.  Глобальные объекты и модули

--- index.js
+++ index.js
@@ -4,6 +4,6 @@
 // const userObj = require('./user');
 // console.log(userObj);
 
-const obj = require('./user');
-console.log(obj.user);
-obj.sayHello();
+// const obj = require('./user');
+// console.log(obj.user);
+// obj.sayHello();
