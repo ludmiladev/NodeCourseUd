a3236ca92a9501eb369bd8302d61fb768c611c4c
ludmiladev <ljuda12@gmail.com>
2021-03-30 10:39:10 +0300

    как получить весь контент

--- refs/fs_ref.js
+++ refs/fs_ref.js
@@ -11,6 +11,44 @@ const path = require('path');
 //     console.log('папка была создана');
 // });
 
+// fs.writeFile(path.join(__dirname, 'notes', 'mynotes.txt'),
+// 'Hello world',
+// (err) => {
+//     if (err) throw err
+//     console.log('файл был создан');
+
+//     fs.appendFile(
+//         path.join(__dirname, 'notes', 'mynotes.txt'),
+//         'From append file',
+//         err => {
+//             if (err) throw err
+//             console.log('файл был изменен');
+//         }
+//     )
+// }
+// )
+
+// метод 1
+// fs.readFile(
+//     path.join(__dirname, 'notes', 'mynotes.txt'),
+//     (err, data) =>{
+//     if (err) throw err
+//     console.log(Buffer.from(data).toString());
+//     }
+// )
+
+// метод 2
+// fs.readFile(
+//     path.join(__dirname, 'notes', 'mynotes.txt'),
+//     'utf-8',
+//     (err, data) =>{
+//     if (err) throw err
+//     console.log(data);
+//     }
+// )
+
+// как получить весь контент 
+
 fs.writeFile(path.join(__dirname, 'notes', 'mynotes.txt'),
 'Hello world',
 (err) => {
@@ -23,26 +61,16 @@ fs.writeFile(path.join(__dirname, 'notes', 'mynotes.txt'),
         err => {
             if (err) throw err
             console.log('файл был изменен');
-        }
-    )
-}
-)
 
-// метод 1
-// fs.readFile(
-//     path.join(__dirname, 'notes', 'mynotes.txt'),
-//     (err, data) =>{
-//     if (err) throw err
-//     console.log(Buffer.from(data).toString());
-//     }
-// )
-
-// метод 2
-fs.readFile(
+    fs.readFile(
     path.join(__dirname, 'notes', 'mynotes.txt'),
     'utf-8',
     (err, data) =>{
     if (err) throw err
     console.log(data);
     }
+)
+        }
+    )
+}
 )
