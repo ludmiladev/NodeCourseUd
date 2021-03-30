78d1a7c2003a9348ba3a4e39972f75fad79e3d46
ludmiladev <ljuda12@gmail.com>
2021-03-30 10:46:14 +0300

    файл переименован

--- refs/fs_ref.js
+++ refs/fs_ref.js
@@ -49,28 +49,41 @@ const path = require('path');
 
 // как получить весь контент 
 
-fs.writeFile(path.join(__dirname, 'notes', 'mynotes.txt'),
-'Hello world',
-(err) => {
-    if (err) throw err
-    console.log('файл был создан');
+// fs.writeFile(path.join(__dirname, 'notes', 'mynotes.txt'),
+// 'Hello world',
+// (err) => {
+//     if (err) throw err
+//     console.log('файл был создан');
 
-    fs.appendFile(
-        path.join(__dirname, 'notes', 'mynotes.txt'),
-        'From append file',
-        err => {
-            if (err) throw err
-            console.log('файл был изменен');
+//     fs.appendFile(
+//         path.join(__dirname, 'notes', 'mynotes.txt'),
+//         'From append file',
+//         err => {
+//             if (err) throw err
+//             console.log('файл был изменен');
 
-    fs.readFile(
-    path.join(__dirname, 'notes', 'mynotes.txt'),
-    'utf-8',
-    (err, data) =>{
-    if (err) throw err
-    console.log(data);
+//     fs.readFile(
+//     path.join(__dirname, 'notes', 'mynotes.txt'),
+//     'utf-8',
+//     (err, data) =>{
+//     if (err) throw err
+//     console.log(data);
+//     }
+// )
+//         }
+//     )
+// }
+// )
+
+//  переименовать
+
+fs.rename(
+    // первій параметр путь к файлу, который мы хотим переименовать
+    path.join(__dirname, 'notes', 'mynotes.txt'), 
+    // второй параметр путь, но с новым названием
+    path.join(__dirname, 'notes', 'notes.txt'), 
+    err => {
+        if(err) throw err
+        console.log('файл переименован');
     }
-)
-        }
-    )
-}
 )
