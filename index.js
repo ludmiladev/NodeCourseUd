93aea7580d47f823bfa79741ec20072a3cef2867
ludmiladev <ljuda12@gmail.com>
2021-03-30 13:43:02 +0300

    подключим HTTP сервер

--- index.js
+++ index.js
@@ -13,3 +13,22 @@
 // console.log('Hello');
 
 // require('./user');
+
+
+
+// подключим HTTP сервер
+const http = require('http');
+
+const server = http.createServer((req, res) =>{
+console.log(req.url)
+
+res.write('<h1>Hello from NodeJS</h1>')
+res.write('<h2>Hello from NodeJS</h2>')
+res.write('<h3>Hello from NodeJS</h3>')
+res.end()
+})
+//  теперь нужно этот сервер запустить
+server.listen(3000, () =>{
+console.log('Server is running...');
+
+})
