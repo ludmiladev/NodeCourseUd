d79d7fbf57989895dbc5363fadfe642084a6479e
ludmiladev <ljuda12@gmail.com>
2021-03-30 14:04:19 +0300

    в метод end можно передавать штмл или текстовый контент и будет работать

--- index.js
+++ index.js
@@ -25,7 +25,11 @@ console.log(req.url)
 res.write('<h1>Hello from NodeJS</h1>')
 res.write('<h2>Hello from NodeJS</h2>')
 res.write('<h3>Hello from NodeJS</h3>')
-res.end()
+res.end(
+    <div style="background: red; width: 200px; height: 200px">
+        <h1>Test</h1>
+    </div>
+)
 })
 //  теперь нужно этот сервер запустить
 server.listen(3000, () =>{
