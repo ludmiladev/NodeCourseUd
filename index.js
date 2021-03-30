92c8c94e50c779e8b685316e816d189461a75d38
ludmiladev <ljuda12@gmail.com>
2021-03-30 15:37:17 +0300

    в метод end можно передавать штмл или текстовый контент в шаблонной строке и будет работать

--- index.js
+++ index.js
@@ -26,9 +26,10 @@ res.write('<h1>Hello from NodeJS</h1>')
 res.write('<h2>Hello from NodeJS</h2>')
 res.write('<h3>Hello from NodeJS</h3>')
 res.end(
-    <div style="background: red; width: 200px; height: 200px">
-        <h1>Test</h1>
-    </div>
+    // <div style="background: red; width: 200px; height: 200px;">
+    //     <h1>Test</h1>
+    // </div>
+    `<div style="background: red; width: 200px; height: 200px;"><h1>Test</h1></div>`
 )
 })
 //  теперь нужно этот сервер запустить
